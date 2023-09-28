25a9690793b52cbae04caa81bf46089cff73169b
Alex Zhauniarovich <aliaksei.zhauniarovich@jaydevs.com>
2023-09-28 02:46:44 +0200

    Initiated and setup demo project, added library usage

---
+++ MoviesListDemoApp/Application/MoviesListDemoAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  MoviesListDemoAppApp.swift
+//  MoviesListDemoApp
+//
+//  Created by Alex Zhauniarovich on 28.09.23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesListDemoAppApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoviesListDemoApp/Presentation/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MoviesListDemoApp
+//
+//  Created by Alex Zhauniarovich on 27.09.23.
+//
+
+import SwiftUI
+import MoviesListLibrary
+
+struct ContentView: View {
+    var body: some View {
+        // Showing "Top 5 Trending movies of the week"
+        MoviesListLibrary().getMoviesListView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
